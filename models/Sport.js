47dc63cfcd595f80f16d50a54f4830bb4c94b7e4
Kesha Hall <k_renee10@yahoo.com>
2022-10-24 11:21:00 -0400

    created sports model

---
+++ models/Sport.js
@@ -0,0 +1,21 @@
+const mongoose = require( "mongoose");
+const Schema = mongoose.Schema;
+
+const SportSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: "Name is required"
+    },
+    price: {
+        type: String,
+        trim: true,
+        required: "Price is required"
+    },
+    items: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Item"
+        }
+    ]
+})
