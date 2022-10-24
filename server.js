a3563b8089c35cd7570fab5afe4e757603aeff1e
Kesha Hall <k_renee10@yahoo.com>
2022-10-24 10:26:18 -0400

    added mongoose connection

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI|| "mongodb://localhost/sports-store-for-the-littles",
+{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true,
+    useFindandModify:false,
+}
+);
+
+const connection = mongoose.connection;
+connection.on("connected", () => {
+    console.log("Mongoose successfully connected.");
+});
+connection.on("error", (err) => {
+    console.log("Mongoose connection error:",err);
+});
 
 
 app.get("/api/config", (req, res) => {
