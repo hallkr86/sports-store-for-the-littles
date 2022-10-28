cd660dcdcc1f04095c394ec231a219acffe34508
Kesha Hall <k_renee10@yahoo.com>
2022-10-28 12:17:49 -0400

    added update and delete route to sport controller

--- controllers/sportController.js
+++ controllers/sportController.js
@@ -17,6 +17,19 @@ router.get("/api/sports", (req, res) => {
     });
 });
 
+router.get("/api/sport/:id", (req, res) => {
+    db.Sport.findById(req.params.id).then(foundSport => {
+        res.json(foundSport);
+    }).catch((err) => {
+        console.log(err);
+        res.json({
+            error: true,
+            data: null,
+            message: `Failed to retreive item with id: ${req.params.id}`,
+        });
+    });
+});
+
 router.post("/api/sports", (req, res) => {
     db.Sport.create(req.body).then((newSport) => {
         res.json(newSport);
@@ -27,6 +40,35 @@ router.post("/api/sports", (req, res) => {
             error: true,
             data: null,
             message: "Failed to create new sport",
+        });
+    });
+});
+
+router.put("/api/sport/:id", (req, res) => {
+    db.Sport.findByIdAndUpdate(req.params.id, req.body, {new: true}).then((updatedSport) => {
+        res.json(updatedSport);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.json({
+            error: true,
+            data: null,
+            message: "Failed to update sport",
+        });
+    });
+});
+
+router.delete("/api/sport/:id", (req, res) => {
+    db.Sport.findByIdAndDelete(req.params.id).then(response => {
+        console.log(response);
+        res.json(response);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.json({
+        error: true,
+        data: null,
+        message: "Failed to delete sport",
         });
     });
 });
