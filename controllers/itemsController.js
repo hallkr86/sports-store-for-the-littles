6ee2737c5756b014984988cfb5a7e1ea5ac95718
Kesha Hall <k_renee10@yahoo.com>
2022-10-28 11:54:02 -0400

    created put and delete routes

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -17,6 +17,19 @@ router.get("/api/items", (req, res) => {
     });
 });
 
+router.get("/api/items/:id", (req, res) => {
+    db.Item.findById(req.params.id).then(foundItem => {
+        res.json(foundItem);
+    }).catch((err) => {
+        console.log(err);
+        res.json({
+            error: true,
+            data: null,
+            message: `Failed to retreive item with id: ${req.params.id}`,
+        });
+    });
+});
+
 router.post("/api/items", (req, res) => {
     db.Item.create(req.body).then((newItem) => {
         res.json(newItem);
@@ -27,6 +40,35 @@ router.post("/api/items", (req, res) => {
             error: true,
             data: null,
             message: "Failed to create new item",
+        });
+    });
+});
+
+router.put("/api/items/:id", (req, res) => {
+    db.Item.findByIdAndUpdate(req.params.id, req.body, {new: true}).then((updatedItem) => {
+        res.json(updatedItem);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.json({
+            error: true,
+            data: null,
+            message: "Failed to update item",
+        });
+    });
+});
+
+router.delete("/api/items/:id", (req, res) => {
+    db.Item.findByIdAndDelete(req.params.id).then(response => {
+        console.log(response);
+        res.json(response);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.json({
+        error: true,
+        data: null,
+        message: "Failed to delete item",
         });
     });
 });
